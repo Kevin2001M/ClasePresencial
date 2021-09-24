74a09564b440c2cd5519fe70e2f2b07653871a72
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2021-09-24 11:48:13 -0600

    Semana 8

---
+++ Usuarios/Usuarios/Controllers/UsuariosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Usuarios.Models;
+
+namespace Usuarios.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+            }
+            else
+            {
+                return View("Index", usuario);
+            }
+
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Usuarios/Usuarios/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usuarios.Models
+{
+    public class Usuario
+    {
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "Error Este campo es requerido")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "Error Este campo es requerido")]
+
+        public int Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Error Este campo es requerido")]
+
+        public char Sexo { get; set; }
+
+        [Display(Name = "Estudio")]
+        [Required(ErrorMessage = "Error Este campo es requerido")]
+
+        public string Estudio { get; set; }
+    }
+}
